40c74bf511a69878a167a27b96299115657d2ff4
Alberto Mendoza Rodríguez <alu0101217741@ull.edu.es>
2022-09-06 21:06:31 +0100

    feat: adds handle error

--- src/services/getDataFromApi.js
+++ src/services/getDataFromApi.js
@@ -4,7 +4,6 @@ const API_BASE_URL = "https://www.datur.me";
 
 export async function getDataFromApi(collectionUrl) {
   try {
-    console.log(`URL: ${API_BASE_URL}${collectionUrl}`);
     const response = await fetch(`${API_BASE_URL}${collectionUrl}`, {
       method: "GET",
       headers: new Headers({
